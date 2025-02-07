39e6d2c13f5ce7c467f254d49c89cda1883e98cc
Kouakouy <93944772+Kouakouy@users.noreply.github.com>
2025-02-08 00:40:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Animation JavaScript
+document.getElementById('action').addEventListener('click', function () {
+    const message = document.getElementById('message');
+    message.classList.toggle('visible');
+});
